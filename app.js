272f8b2a56b4a28a8bc1b3ba738a8433e714f589
JinahFromBusan <jinahhwang245@gmail.com>
2022-12-03 23:59:00 +0900

    add update func

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const fs = require('fs');
 const multer = require('multer');
+const methodOverride = require('method-override');
 
-const mysqldb = mysql.createConnection({
-    user     : 'root',
-    password : '1111',
-    database : 'AWS'
-});
+// const mysqldb = mysql.createConnection({
+//     user     : 'root',
+//     password : '1111',
+//     database : 'AWS'
+// });
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.static('views'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+
+// 첨부파일 설정
 let filename;
 let storage = multer.diskStorage({
     destination : function(req, file, cb){
@@ -29,8 +42,9 @@ let storage = multer.diskStorage({
         filesize: 1024 * 1024
     }
 });
-
 let upload = multer({ storage : storage });
+
+// mongoDB 설정
 let db;
 const dbName = 'gallery';
 mongodb.connect('mongodb+srv://admin:qwer1234@cluster0.xukw2sd.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
@@ -43,14 +57,6 @@ mongodb.connect('mongodb+srv://admin:qwer1234@cluster0.xukw2sd.mongodb.net/?retr
     })
 })
 
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-app.use(express.static('views'));
-app.use('/public', express.static(__dirname + '/public'));
 
 //메인화면
 app.get('/', (req, res) => {
@@ -78,20 +84,21 @@ app.get('/list', (req, res) => {
 app.get('/detail/:id', (req, res) => {
     db.collection('post').findOne({_id: parseInt(req.params.id)}, (err, result) => {
         if (err) return console.log(err);
-        // console.log(result.img);
         res.sendFile(__dirname + 'public/image' + filename);
         res.render('detail.ejs', {post : result});
     });
 });
 
 //수정화면
-app.get('/update', (req, res) => {
+app.get('/update/:id', (req, res) => {
     db.collection('post').findOne({_id : parseInt(req.params.id)}, (err, result) => {
         if (err) return console.log(err);
+        res.sendFile(__dirname + '/public/image' + filename);
         res.render('update.ejs', { post : result});
     });
 });
 
+// 신규저장
 app.post('/add', upload.single('imgFile'), (req, res) => {
     db.collection('counter').findOne({name: '게시물갯수'}, (err, result) => {
         // 카운터 불러오기
@@ -110,13 +117,21 @@ app.post('/add', upload.single('imgFile'), (req, res) => {
     });
 });
 
+// 업데이트
+app.put('/update', (req, res) => {
+    db.collection('post').updateOne({ _id : parseInt(req.body.id)}, { $set : { title : req.body.title, content : req.body.content }}, (err, result) => {
+        if(err) return console.log(err);
+        res.redirect('/list');
+    });
+});
+
 //삭제요청
 app.delete('/delete', (req, res) => {
     req.body._id = parseInt(req.body._id);
-    console.log(req.body._id)
     db.collection('post').deleteOne(req.body, (err, result) => {
+        if(err) return console.log(err);
         console.log('삭제완료');
-        res.status(200).send({ message : '삭제완료'});
-        res.redirect('/list');
-    })
-})
+        })
+        //res.status(200).send({ message : '삭제완료'});
+    res.redirect('/list');
+});
