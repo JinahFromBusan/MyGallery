b74ee734b9d40c9dc8dd7f2deb1fdc42c1b13fb6
JinahFromBusan <jinahhwang245@gmail.com>
2022-12-05 17:28:38 +0900

    .

--- routes/get.js
+++ routes/get.js
@@ -1,6 +1,13 @@
 let router = require('express').Router();
 
 
-
+//메인화면
+router.get('/', (req, res) => {
+    db.collection('post').find().toArray((err, result) => {
+        if (err) return console.log("메인화면 : " + err);
+        res.sendFile(__dirname + "/public/image/" + filename);
+        res.render('index.ejs', {posts : result });
+    })
+});
 
 module.exports = router;
